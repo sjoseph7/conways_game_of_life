9e6acd6172afb2bc71febbf8d685d16dee445a2c
sjoseph7 <sjoseph7.git@gmail.com>
2020-08-30 21:35:33 -0700

    feat: convert 'App' into functional component
    Note: Stop button now stays on after run is hit immediately after reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import Grid from "./components/Grid";
 import Button from "react-bootstrap/Button";
 import ButtonToolbar from "react-bootstrap/ButtonToolbar";
@@ -17,65 +17,57 @@ const LOW_COLOR = [79, 115, 54];
 const HIGH_COLOR = [170, 58, 56];
 const DEFAULT_COLOR = gradient(LOW_COLOR, HIGH_COLOR, 0); //this.colors[this.colors.length - 1];
 
-interface AppState {
-  generation: number;
-  cellStates: boolean[][];
-  cellColors: string[][];
-  stepInterval: NodeJS.Timeout | null;
-}
-
-export default class App extends React.Component<{}, AppState> {
-  state: AppState = {
-    generation: 0,
-    cellStates: this.createEmptyGrid(),
-    cellColors: this.createEmptyGrid(),
-    stepInterval: null
-  };
+const _App = () => {
+  const [generation, setGeneration] = useState(0);
+  const [cellStates, setCellStates] = useState(createEmptyGrid(false));
+  const [cellColors, setCellColors] = useState(createEmptyGrid(""));
+  const [stepInterval, setStepInterval] = useState(
+    null as NodeJS.Timeout | null
+  );
 
   // ---- Life-Cycle Methods ---- //
-  componentDidMount() {
-    this.seedGrid(2);
-  }
+  useEffect(() => {
+    seedGrid(2);
+  }, []);
 
   // ---- Handlers ---- //
-  handleSelect = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const handleSelect = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
     const { id } = event.target as HTMLElement;
-    const [r, c] = id.split("_");
-    const row = parseInt(r);
-    const col = parseInt(c);
+    const [row, col] = id.split("_");
+    const selectedRow = parseInt(row);
+    const selectedCol = parseInt(col);
 
-    // const cellID = getID(this.state.cellStates, row, col);
+    // Toggle the state of the selected cell
+    setCellStates(cellStates =>
+      cellStates.map((cellStatesRow: boolean[], row_index: number) => {
+        return cellStatesRow.map((cellState: boolean, col_index: number) => {
+          return row_index === selectedRow && col_index === selectedCol
+            ? !cellState
+            : cellState;
+        });
+      })
+    );
 
-    this.setState((prevState: any) => {
-      return {
-        cellStates: prevState.cellStates.map(
-          (cellStatesRow: boolean[], row_index: number) => {
-            return cellStatesRow.map(
-              (cellState: boolean, col_index: number) => {
-                return row === row_index && col === col_index
-                  ? !cellState
-                  : cellState;
-              }
-            );
-          }
-        ),
-        cellColors: prevState.cellColors.map(
-          (cellColorsRow: string[], row_index: number) => {
-            return cellColorsRow.map((cellColor: string, col_index: number) => {
-              return row === row_index && col === col_index
-                ? !cellColor
-                : cellColor;
-            });
-          }
-        )
-      };
-    });
+    // Toggle the color of the selected cell
+    setCellColors(cellColors =>
+      cellColors.map((cellColorsRow: string[], row_index: number) => {
+        return cellColorsRow.map((cellColor: string, col_index: number) => {
+          return row_index === selectedRow && col_index === selectedCol
+            ? cellColor
+              ? ""
+              : DEFAULT_COLOR
+            : cellColor;
+        });
+      })
+    );
   };
 
   // ---- Setup Methods ---- //
-  seedGrid = (odds: number = 2) => {
-    let seededStateGrid = this.createEmptyGrid();
-    let seededColorGrid = this.createEmptyGrid();
+  const seedGrid = (odds: number = 2) => {
+    let seededStateGrid = createEmptyGrid(false);
+    let seededColorGrid = createEmptyGrid("");
 
     for (let row = 0; row < ROWS; row++) {
       for (let col = 0; col < COLS; col++) {
@@ -86,172 +78,151 @@ export default class App extends React.Component<{}, AppState> {
         }
       }
     }
-
-    this.setState({
-      cellStates: seededStateGrid,
-      cellColors: seededColorGrid
-    });
+    setCellStates(seededStateGrid);
+    setCellColors(seededColorGrid);
   };
 
-  createEmptyGrid(): any[][] {
+  function createEmptyGrid<T extends any>(filler: T): T[][] {
     return Array(ROWS)
       .fill(0)
-      .map(() => Array(COLS).fill(false));
+      .map(() => Array(COLS).fill(filler));
   }
 
   // ---- Actions ---- //
-  step = () => {
-    this.setState(
-      (prevState: AppState): AppState => {
-        let nextCellStates: boolean[][] = [];
-        for (let row = 0; row < ROWS; row++) {
-          let nextCellStatesRow: boolean[] = [];
-          for (let col = 0; col < COLS; col++) {
-            let numberOfNeighbors = countNeighbors(
-              prevState.cellStates,
-              row,
-              col
-            );
-            let cellState = prevState.cellStates[row][col]
-              ? numberOfNeighbors === 2 || numberOfNeighbors === 3
-              : numberOfNeighbors === 3;
-            nextCellStatesRow.push(cellState);
-          }
-          nextCellStates.push(nextCellStatesRow);
+  const step = () => {
+    setCellStates(cellStates => {
+      let nextCellStates: boolean[][] = [];
+      for (let row = 0; row < ROWS; row++) {
+        let nextCellStatesRow: boolean[] = [];
+        for (let col = 0; col < COLS; col++) {
+          let numberOfNeighbors = countNeighbors(cellStates, row, col);
+          let cellState = cellStates[row][col]
+            ? numberOfNeighbors === 2 || numberOfNeighbors === 3
+            : numberOfNeighbors === 3;
+          nextCellStatesRow.push(cellState);
         }
-
-        // Stop when nothing is changing
-        if (
-          JSON.stringify(prevState.cellStates) ===
-          JSON.stringify(nextCellStates)
-        ) {
-          this.stop();
-          return {} as AppState;
-        }
-
-        // Color code the "creatures"
-        const uf = new unionFind(ROWS * COLS + 1);
-        const activeCellCoordinates = getActiveCellCoordinates(nextCellStates);
-        const nextCellColors = this.createEmptyGrid();
-
-        activeCellCoordinates.forEach(([row, col]: number[]) => {
-          const neighborCoordinates = getActiveNeighborCoordinates(
-            nextCellStates,
-            row,
-            col
-          );
-          neighborCoordinates.forEach(coord => {
-            let [n_row, n_col] = coord;
-            const id = getID(prevState.cellStates, row, col);
-            const n_id = getID(prevState.cellStates, n_row, n_col);
-            uf.union(id, n_id);
-          });
-        });
-
-        activeCellCoordinates.forEach(([row, col]: number[]) => {
-          const cellID = getID(prevState.cellStates, row, col);
-          const cellRoot = uf.root(cellID);
-          const sizePercent = (uf.segmentSize(cellRoot) / (ROWS * COLS)) * 10;
-          const selectedColor = gradient(LOW_COLOR, HIGH_COLOR, sizePercent);
-
-          nextCellColors[row][col] = selectedColor;
-        });
-
-        return {
-          cellStates: nextCellStates,
-          cellColors: nextCellColors,
-          generation: prevState.generation + 1,
-          stepInterval: prevState.stepInterval
-        };
+        nextCellStates.push(nextCellStatesRow);
       }
-    );
-  };
 
-  run = () => {
-    if (!this.state.stepInterval) {
-      this.setState({ stepInterval: setInterval(this.step, STEP_DELAY) });
-    }
-  };
+      // Stop when nothing is changing
+      if (JSON.stringify(cellStates) === JSON.stringify(nextCellStates)) {
+        stop();
+        return cellStates;
+      }
 
-  stop = () => {
-    if (!!this.state.stepInterval) {
-      clearInterval(this.state.stepInterval);
-    }
-    this.setState({ stepInterval: null });
-  };
+      // Color code the groups of cells
+      const uf = new unionFind(ROWS * COLS + 1);
+      const activeCellCoordinates = getActiveCellCoordinates(nextCellStates);
+      const nextCellColors = createEmptyGrid("");
 
-  reset = () => {
-    if (!!this.state.stepInterval) {
-      clearInterval(this.state.stepInterval);
-    }
-    this.setState({
-      generation: 0,
-      cellStates: this.createEmptyGrid(),
-      cellColors: this.createEmptyGrid(),
-      stepInterval: null
+      activeCellCoordinates.forEach(([row, col]: number[]) => {
+        const neighborCoordinates = getActiveNeighborCoordinates(
+          nextCellStates,
+          row,
+          col
+        );
+        neighborCoordinates.forEach(coord => {
+          let [n_row, n_col] = coord;
+          const id = getID(cellStates, row, col);
+          const n_id = getID(cellStates, n_row, n_col);
+          uf.union(id, n_id);
+        });
+      });
+
+      activeCellCoordinates.forEach(([row, col]: number[]) => {
+        const cellID = getID(cellStates, row, col);
+        const cellRoot = uf.root(cellID);
+        const sizePercent = (uf.segmentSize(cellRoot) / (ROWS * COLS)) * 10;
+        const selectedColor = gradient(LOW_COLOR, HIGH_COLOR, sizePercent);
+
+        nextCellColors[row][col] = selectedColor;
+      });
+
+      setCellColors(nextCellColors);
+      setGeneration(generation => generation + 1);
+      setStepInterval(stepInterval => stepInterval);
+
+      return nextCellStates;
     });
   };
 
-  render() {
-    return (
-      <div className="center">
-        <h1>
-          Conway's
-          <br />
-          Game of Life
-        </h1>
-        <ButtonToolbar style={{ textAlign: "center" }} className="center">
-          <Button
-            style={{ margin: "5px" }}
-            variant="warning"
-            onClick={() => this.seedGrid()}
-          >
-            Seed
-          </Button>
-          <Button
-            style={{ margin: "5px" }}
-            variant="primary"
-            onClick={this.step}
-            disabled={!!this.state.stepInterval}
-          >
-            Step
-          </Button>
-          <Button
-            style={{ margin: "5px" }}
-            variant="success"
-            onClick={this.run}
-            disabled={!!this.state.stepInterval}
-          >
-            Run
-          </Button>
-          <Button
-            style={{ margin: "5px" }}
-            variant="danger"
-            onClick={this.stop}
-            disabled={!this.state.stepInterval}
-          >
-            Stop
-          </Button>
-          <Button
-            style={{ margin: "5px" }}
-            variant="secondary"
-            onClick={this.reset}
-          >
-            Reset
-          </Button>
-        </ButtonToolbar>
-        <Grid
-          rows={ROWS}
-          cols={COLS}
-          cellStates={this.state.cellStates}
-          cellColors={this.state.cellColors}
-          handleSelect={this.handleSelect}
-        />
-        <h2>Generation: {this.state.generation}</h2>
-      </div>
-    );
-  }
-}
+  const run = () => {
+    stepInterval && clearInterval(stepInterval);
+    setStepInterval(setInterval(step, STEP_DELAY));
+  };
+
+  const stop = () => {
+    if (stepInterval) {
+      clearInterval(stepInterval);
+      setStepInterval(null);
+    }
+  };
+
+  const reset = () => {
+    stepInterval && clearInterval(stepInterval);
+
+    setGeneration(0);
+    setCellStates(createEmptyGrid(false));
+    setCellColors(createEmptyGrid(""));
+    setStepInterval(null);
+  };
+
+  return (
+    <div className="center">
+      <h1>
+        Conway's
+        <br />
+        Game of Life
+      </h1>
+      <ButtonToolbar style={{ textAlign: "center" }} className="center">
+        <Button
+          style={{ margin: "5px" }}
+          variant="warning"
+          onClick={() => seedGrid()}
+        >
+          Seed
+        </Button>
+        <Button
+          style={{ margin: "5px" }}
+          variant="primary"
+          onClick={step}
+          disabled={!!stepInterval}
+        >
+          Step
+        </Button>
+        <Button
+          style={{ margin: "5px" }}
+          variant="success"
+          onClick={run}
+          disabled={!!stepInterval}
+        >
+          Run
+        </Button>
+        <Button
+          style={{ margin: "5px" }}
+          variant="danger"
+          onClick={stop}
+          disabled={!stepInterval}
+        >
+          Stop
+        </Button>
+        <Button style={{ margin: "5px" }} variant="secondary" onClick={reset}>
+          Reset
+        </Button>
+      </ButtonToolbar>
+      <Grid
+        rows={ROWS}
+        cols={COLS}
+        cellStates={cellStates}
+        cellColors={cellColors}
+        handleSelect={handleSelect}
+      />
+      <h2>Generation: {generation}</h2>
+    </div>
+  );
+};
+
+export default _App;
 
 function countNeighbors(grid: boolean[][], cellRow: number, cellCol: number) {
   if (!grid) {
